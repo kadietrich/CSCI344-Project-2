a0de6caf4da87761b8523b7a6a13109c632aaef6
Kurt Dietrich <kurt.dietrich0801@gmail.com>
2013-02-23 15:02:10 -0500

    Add input to index.html. Copy basic ctwitter function from Lab2 to app.js as a starting point.

---
+++ javascripts/app/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  var twitter = new ctwitter.CTwitter();
+  twitter.stream("statuses/filter", { lang:"en", track:[] }, function (stream){
+    stream.on("data", function (tweet){
+		  $('<p class="tweet"><img src="'+tweet.profile_image_url+'" />'+tweet.text+'</p>')
+		    .hide()
+		    .prependTo('#tweets')
+        .fadeIn(500)
+		    .slideDown(50);
+		  $('#tweets p:gt(9)')
+		    .fadeOut(500, function() { $(this).remove() });
+    });
+  });
+});
