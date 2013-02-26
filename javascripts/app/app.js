0ca2257070834bd27ce8abb1ef9b691b86e7b7a5
Kurt Dietrich <kurt.dietrich0801@gmail.com>
2013-02-25 21:03:47 -0500

    Add some basic styles to page. Add basic search functionanlity and search results display.

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -1,14 +1,26 @@
 $(document).ready(function(){
-  var twitter = new ctwitter.CTwitter();
-  twitter.stream("statuses/filter", { lang:"en", track:[] }, function (stream){
-    stream.on("data", function (tweet){
-		  $('<p class="tweet"><img src="'+tweet.profile_image_url+'" />'+tweet.text+'</p>')
-		    .hide()
-		    .prependTo('#tweets')
-        .fadeIn(500)
-		    .slideDown(50);
-		  $('#tweets p:gt(9)')
-		    .fadeOut(500, function() { $(this).remove() });
+    $("#search_input").val("Enter a search term");
+    $("#search_input_button").click(function(){
+      twitterFunction($("#search_input").val());
+      //$("#results").html($("#search_input").val());
+      $("#search_form").fadeOut(500, function(){
+        $("#results").fadeIn(500);
+      });
     });
-  });
+
+  var twitterFunction = function(searchTerm){
+    var twitter = new ctwitter.CTwitter();
+    twitter.stream("statuses/filter", { lang:"en", track:searchTerm }, function (stream){
+      stream.on("data", function (tweet){
+		    $('<p class="tweet"><img src="'+tweet.profile_image_url+'" />'+tweet.text+'</p>').hide()
+        .prependTo('#results')
+        .fadeIn(500)
+        .slideDown(50);
+		    $('#results p:gt(3)').fadeOut(500, function(){ $(this).remove()});
+      });
+    });
+  };
+  
+  
+  
 });
