da28d0d0b041d191e852d9e827fcb807cf6213a8
Kurt Dietrich <kurt.dietrich0801@gmail.com>
2013-02-26 20:57:50 -0500

    Add basic counter for number of tweets seen. Reduce ctwitter stream buffer by 1 second.

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -2,12 +2,12 @@ $(document).ready(function(){
     $("#search_input").val("Enter a search term");
     $("#search_input_button").click(function(){
       twitterFunction($("#search_input").val());
-      //$("#results").html($("#search_input").val());
       $("#search_form").fadeOut(500, function(){
         $("#results").fadeIn(500);
+        $("#tweetCounter").fadeIn(500);
       });
     });
-
+  var count = 0;
   var twitterFunction = function(searchTerm){
     var twitter = new ctwitter.CTwitter();
     twitter.stream("statuses/filter", { lang:"en", track:searchTerm }, function (stream){
@@ -17,10 +17,18 @@ $(document).ready(function(){
         .fadeIn(500)
         .slideDown(50);
 		    $('#results p:gt(3)').fadeOut(500, function(){ $(this).remove()});
+        count += 1;
+        $("#tweetCounter").html($("<p class='counter'>Tweets seen: "+count+"</p>"));
+        
       });
     });
   };
-  
-  
-  
+  /*  var count = function(){
+          var increment = 0;
+          return function(){ return increment += 1; };
+        };
+        var ob = count();
+        $("<p class='counter'>"+ob+"</p>").appendTo("#tweetCounter");
+        $(".counter").replaceWith("<p class='counter'>"+ob+"</p>");
+   */
 });
--- javascripts/lib/ctwitter/build/ctwitter.js
+++ javascripts/lib/ctwitter/build/ctwitter.js
@@ -301,7 +301,7 @@ if(!window.ctwitter) window.ctwitter = { };
 		    lastID = buffer[0].id_str;
                     bufferTimeout = setTimeout(function () {
 			deliverData(stream);
-                    }, 5000);
+                    }, 4000);
 		} else {
 		    isStreaming = false;
 		}
