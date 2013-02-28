f4e8c0b93599af8c364b31eb81c822598db7c6eb
Kurt Dietrich <kurt.dietrich0801@gmail.com>
2013-02-27 19:02:46 -0500

    Fix typos in index.html and app.css. Restructure app.js to please JSLINT.

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -1,34 +1,34 @@
-$(document).ready(function(){
-    $("#search_input").val("Enter a search term");
-    $("#search_input_button").click(function(){
-      twitterFunction($("#search_input").val());
-      $("#search_form").fadeOut(500, function(){
-        $("#results").fadeIn(500);
-        $("#tweetCounter").fadeIn(500);
-      });
-    });
-  var count = 0;
-  var twitterFunction = function(searchTerm){
-    var twitter = new ctwitter.CTwitter();
-    twitter.stream("statuses/filter", { lang:"en", track:searchTerm }, function (stream){
-      stream.on("data", function (tweet){
-		    $('<p class="tweet"><img src="'+tweet.profile_image_url+'" />'+tweet.text+'</p>').hide()
-        .prependTo('#results')
-        .fadeIn(500)
-        .slideDown(50);
-		    $('#results p:gt(3)').fadeOut(500, function(){ $(this).remove()});
-        count += 1;
-        $("#tweetCounter").html($("<p class='counter'>Tweets seen: "+count+"</p>"));
-        
-      });
-    });
-  };
-  /*  var count = function(){
-          var increment = 0;
-          return function(){ return increment += 1; };
+(function () {
+    "use strict";
+    var $ = window.$,
+        main;
+    main = function () {
+        var twitterFunction = function (searchTerm) {
+            var twitter = new ctwitter.CTwitter();
+            twitter.stream('statuses/filter', { lang: 'en', track: searchTerm }, function (stream) {
+                var count = 0;
+                stream.on('data', function (tweet) {
+                    $('<p class="tweet"><img src="' + tweet.profile_image_url + '" />' + tweet.text + '</p>').hide()
+                        .prependTo('#results')
+                        .fadeIn(500)
+                        .slideDown(50);
+                    $('#results p:gt(4)').fadeOut(500, function () { $(this).remove(); });
+                    count += 1;
+                    $('#tweetCounter').html($("<p class='counter'>Tweets seen: " + count + "</p>"));
+                });
+            });
         };
-        var ob = count();
-        $("<p class='counter'>"+ob+"</p>").appendTo("#tweetCounter");
-        $(".counter").replaceWith("<p class='counter'>"+ob+"</p>");
-   */
-});
+        $('#search_input').val("Enter a search term");
+        $('#search_input_button').click(function () {
+            twitterFunction($("#search_input").val());
+            var term = $('#search_input').val();
+            $('#results_header').html("<h1>Tweets about: " + term + "</h1>");
+            $('#search_form').fadeOut(500, function () {
+                $('#results').fadeIn(500);
+                $('#tweetCounter').fadeIn(500);
+            });
+        });
+    };
+    $(document).ready(main);
+    window.main = main;
+}());
